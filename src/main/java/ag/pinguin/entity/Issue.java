bffc1e631b7971d5ab9cc25c627b268568bf32f9
Emiliia Nesterovych <emiliia.nesterovych@gmail.com>
2020-08-25 01:29:58 +0300

    Add test data for application

--- src/main/java/ag/pinguin/entity/Issue.java
+++ src/main/java/ag/pinguin/entity/Issue.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Issue {
     @Id
     @GeneratedValue
---
+++ src/main/resources/ data.sql
@@ -0,0 +1,30 @@
+INSERT INTO DEVELOPER
+(developer_id, first_Name)
+VALUES
+('3b43c8c6e5f711eaadc10242ac120001', 'Mary'),
+('3b43c8c6e5f711eaadc10242ac120004', 'Ben' ),
+('3b43c8c6e5f711eaadc10242ac120005', 'Harold');
+
+INSERT INTO STORY
+(id, title, description, estimate, status, creation_date)
+VALUES
+('4a43c8c6e5f711eaadc10242ac120001', 'Implement some algorithm', 'A description of algorithm with formulas', 1, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120002', 'Implement some algorithm', 'A description of algorithm with formulas', 1, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120003', 'Implement some algorithm', 'A description of algorithm with formulas', 1, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120004', 'Implement some algorithm', 'A description of algorithm with formulas', 1, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120005', 'Implement some algorithm', 'A description of algorithm with formulas', 2, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120006', 'Implement some algorithm', 'A description of algorithm with formulas', 2, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120007', 'Implement some algorithm', 'A description of algorithm with formulas', 2, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120008', 'Implement some algorithm', 'A description of algorithm with formulas', 2, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120009', 'Implement some algorithm', 'A description of algorithm with formulas', 3, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac120010', 'Implement some algorithm', 'A description of algorithm with formulas', 3, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130001', 'Implement some algorithm', 'A description of algorithm with formulas', 3, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130002', 'Implement some algorithm', 'A description of algorithm with formulas', 3, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130003', 'Implement some algorithm', 'A description of algorithm with formulas', 5, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130004', 'Implement some algorithm', 'A description of algorithm with formulas', 5, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130005', 'Implement some algorithm', 'A description of algorithm with formulas', 5, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130006', 'Implement some algorithm', 'A description of algorithm with formulas', 5, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130007', 'Implement some algorithm', 'A description of algorithm with formulas', 8, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130008', 'Implement some algorithm', 'A description of algorithm with formulas', 8, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130009', 'Implement some algorithm', 'A description of algorithm with formulas', 8, 'ESTIMATED', CURRENT_TIMESTAMP()),
+('4a43c8c6e5f711eaadc10242ac130010', 'Implement some algorithm', 'A description of algorithm with formulas', 20, 'ESTIMATED', CURRENT_TIMESTAMP());
